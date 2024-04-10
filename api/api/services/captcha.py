2bc83daeedd28163a9ba10258fac098150df4ff3
Giacomo Licari <giacomo@gnosis.io>
2024-04-10 16:56:45 +0200

    Log captcha verify response

--- api/api/services/captcha.py
+++ api/api/services/captcha.py
@@ -1,4 +1,8 @@
 import requests
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 def captcha_verify(client_response, catpcha_api_url, secret_key, remote_ip, site_key):
@@ -8,6 +12,8 @@ def captcha_verify(client_response, catpcha_api_url, secret_key, remote_ip, site
         'remoteip': remote_ip,
         'sitekey': site_key
     })
+
+    logging.info('Captcha verify response: %s' % request.json())
 
     if request.status_code != 200:
         return False
